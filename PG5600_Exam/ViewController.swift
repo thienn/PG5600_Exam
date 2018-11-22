4b32d8c641a124044d2fb675295a76152cc656a8
thienn <thien.oslo@gmail.com>
2018-11-22 03:59:13 +0100

    Initial commit

---
+++ PG5600_Exam/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PG5600_Exam
+//
+//  Created by Thien Nguyen on 22/11/2018.
+//  Copyright © 2018 Thien Cong Pham. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ PG5600_ExamTests/PG5600_ExamTests.swift
@@ -0,0 +1,34 @@
+//
+//  PG5600_ExamTests.swift
+//  PG5600_ExamTests
+//
+//  Created by Thien Nguyen on 22/11/2018.
+//  Copyright © 2018 Thien Cong Pham. All rights reserved.
+//
+
+import XCTest
+@testable import PG5600_Exam
+
+class PG5600_ExamTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
