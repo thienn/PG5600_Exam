bfadf717db44e51dd47baeb590b874045024e8ed
thienn <thien.oslo@gmail.com>
2018-11-26 08:19:04 +0100

    Add & Delete Films from CoreData + check
    + When the view is loaded, check in with the CoreData context to see if there is any records in there with the title of the current film.title for checks
    + Add or delete record of film in the CoreData "Films" entity.
    + If check that will show the add or delete button, and another that will call the right function addFilm / deleteFilm
    ---
    + Removed the constraints in Details for now. Will fix them later to show crawl etc.

--- PG5600_Exam/Controller/DetailFilmViewController.swift
+++ PG5600_Exam/Controller/DetailFilmViewController.swift
@@ -5,6 +5,8 @@
 //  Created by Thien Nguyen on 22/11/2018.
 //  Copyright © 2018 Thien Cong Pham. All rights reserved.
 //
+// Examples on how to setup CoreData + CRUD from lectures + https://medium.com/@ankurvekariya/core-data-crud-with-swift-4-2-for-beginners-40efe4e7d1cc
+//
 
 import UIKit
 import CoreData
@@ -12,9 +14,6 @@ import CoreData
 class DetailFilmViewController: UIViewController {
     
     var film: Film!
-    
-
-    @IBOutlet weak var stackBackground: UIView!
     
     @IBOutlet weak var titleLabel: UILabel!
     @IBOutlet weak var episodeLabel: UILabel!
@@ -27,9 +26,12 @@ class DetailFilmViewController: UIViewController {
     
     @IBOutlet weak var favoriteButton: UIButton!
     
+    var checkStatus = false;
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        // Do any additional setup after loading the view.
         crawlText.isEditable = false
     
         // Connect the data sent in with the labels
@@ -39,20 +41,53 @@ class DetailFilmViewController: UIViewController {
         producerLabel.text = film.producer
         releaseDateLabel.text = film.releaseDate
         crawlText.text = film.crawl
+ 
         
-        stackBackground.layer.cornerRadius = 5
-        // Do any additional setup after loading the view.
-
-
+        // Connect to the CoreData to check if the current Title exist in the records or not
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        let context = appDelegate.persistentContainer.viewContext
+        
+        let request = NSFetchRequest<NSFetchRequestResult>(entityName: "Films")
+        
+        do {
+            let result = try context.fetch(request)
+            for data in result as! [NSManagedObject] {
+                print(data.value(forKey: "title") as! String)
+                // If the title exist in the records turn the checkStatus true
+                // This will ensure that whenever the user goes in and out of the details, it will run the check always. Will try to move it out of the for loop if possible later
+                if data.value(forKey: "title") as! String == film.title {
+                    checkStatus = true
+                }
+            }
+             print("---")
+        } catch {
+            print("Failed to get data")
+        }
+        
+        // If it exist, then show the button as delete else default will be Add
+        if checkStatus == true {
+            favoriteButton.setTitle("Delete from favorite", for: .normal)
+        } else {
+            favoriteButton.setTitle("Add to favorite", for: .normal)
+        }
     }
     
     // When clicking the button it should add or remove from the Core data
     @IBAction func addToDB(_ sender: UIButton) {
-        sender.setTitle("Add to favorite", for: .normal)
-        
+        // Convert to D.R.Y when possible for Characters and Films
+        if checkStatus == true {
+            deleteFilm()
+        } else {
+            addFilm()
+        }
+    }
+    
+    func addFilm() {
+        // Creates the context, then new object to insert into the context (CoreData)
+        // Then save it, and refresh the view
         let appDelegate = UIApplication.shared.delegate as! AppDelegate
         let context = appDelegate.persistentContainer.viewContext
-        
+ 
         let entity = NSEntityDescription.entity(forEntityName: "Films", in: context)
         let newFilm = NSManagedObject(entity: entity!, insertInto: context)
         
@@ -65,24 +100,44 @@ class DetailFilmViewController: UIViewController {
         
         do {
             try context.save()
+            print("Added \(film.title) to CoreData")
+            self.viewDidLoad() // to refresh view for button
         } catch {
             print("Failed to save to DB")
         }
+    }
+    
+    // function for delete
+    func deleteFilm() {
+        // Creates the context, then fetch the data from the context (CoreData)
+        // with the key title (Like SQL queries where title ==). Then delete that record
+        // as it should only return 1 record, therefore the index 0 should always be right
+        // then save it, and refresh the view
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        let context = appDelegate.persistentContainer.viewContext
         
-        let request = NSFetchRequest<NSFetchRequestResult>(entityName: "Films")
-        request.returnsObjectsAsFaults = false
+        let fetchRequest = NSFetchRequest<NSFetchRequestResult>(entityName: "Films")
+        fetchRequest.predicate = NSPredicate(format: "title =%@", film.title)
         
         do {
-            let result = try context.fetch(request)
-            for data in result as! [NSManagedObject] {
-                print(data.value(forKey: "title") as! String)
+            let test = try context.fetch(fetchRequest)
+            let objectToDelete = test[0] as! NSManagedObject
+            context.delete(objectToDelete)
+            
+            do {
+                print("Deleted \(film.title) from CoreData")
+                try context.save()
+                checkStatus = false
+                self.viewDidLoad() // to refresh view for button
+            } catch {
+                print(error)
             }
+          
         } catch {
-            print("Failed to get data")
+            print(error)
         }
         
     }
-    
     
 
     /*
